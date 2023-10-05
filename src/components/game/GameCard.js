4495b6a3b88c5daca12395a6df1f1edb20efc5f0
Navid Yazdani <mohammadYazdani7@gmail.com>
2023-10-04 17:41:53 -0700

    every sits in the table data

--- src/components/game/GameCard.js
+++ src/components/game/GameCard.js
@@ -2,38 +2,48 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './GameCard.css'; // Make sure you have a CSS file for styling
 
-function CharacterList() {
+function CharacterList({playerChar}) {
   const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // Make an API request to retrieve character data
     axios.get('http://localhost:3500/api/game/character')
+
       .then((response) => {
         setCharacters(response.data);
       })
       .catch((error) => {
         console.error('Error fetching characters:', error);
       });
-  }, []);
+  }, [playerChar]);
+console.log(characters);
 
+
+console.log('playerchar',playerChar);
+  
   return (
     <div>
       <h2>Character List</h2>
       <ul className="character-card-container"> {/* Add a container for the character cards */}
         {characters.map((character) => (
-          <li key={character._id}>
-            <div className="character-card"> {/* Wrap each character card in a div */}
-              <img
-                className="character-image"
-                src={`data:${character.image.contentType};base64,${arrayBufferToBase64(character.image.data.data)}`}
-                alt={character.name}
-              />
-              <div className="character-info">
-                <h3 className="character-name">{character.name}</h3>
-                <p className="character-ability">Ability: {character.ability}</p>
+          <>
+          {character.name = playerChar ? (
+              <li key={character.id} >
+              <div className="character-card"> {/* Wrap each character card in a div */}
+                <img
+                  className="character-image"
+                  src={`data:${character.image.contentType};base64,${arrayBufferToBase64(character.image.data.data)}`}
+                  alt={character.name}
+                />
+                <div className="character-info">
+                  <h3 className="character-name">{character.name}</h3>
+                  <p className="character-ability">Ability: {character.ability}</p>
+                </div>
               </div>
-            </div>
-          </li>
+            </li>
+          ) : (
+            'theres something wrong'
+          )}</>
         ))}
       </ul>
     </div>
--- src/components/game/Table.js
+++ src/components/game/Table.js
@@ -2,56 +2,40 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import jwt_decode from 'jwt-decode';
 import { useParams } from "react-router-dom";
-import './GameCard.css';
-function GameTable () {
-    const [data, setData] = useState([]);
+import './GameCard.css'
+import GameCard from './GameCard.js';
 
-    const token = localStorage.getItem('user')
+function GameTable() {
+  const [data, setData] = useState([]);
+    const [charId, setCharId] =useState('');
 
-    const userInfo = jwt_decode(token)
-    const userId = userInfo.userId
-    const {gameKey} = useParams()
-   
+  const token = localStorage.getItem('user')
+  const userInfo = jwt_decode(token)
+  const userId = userInfo.userId
+  const { gameKey } = useParams()
 
-    useEffect(() => {
-    axios.post(`http://localhost:3500/api/game/table`,{
-        gameKey: gameKey,
-        userId: userId,
+  useEffect(() => {
+    axios.post(`http://localhost:3500/api/game/table`, {
+      gameKey: gameKey,
+      userId: userId,
     })
-      .then((response) => {
-       setData(response.data);
-        
-      })
-      
-      .catch((error) => {
-        console.error('Error fetching user lobbies:', error);
-      });
-  }, []);
-  
-    
-    console.log(data)
-return(
+    .then((response) => {
+      setData(response.data);
+      setCharId(response.data[0].char)
+    })
+    .catch((error) => {
+      console.error('Error fetching user lobbies:', error);
+    });
+  }, [gameKey, userId]);
+
+console.log('charId', charId);
+  console.log(data);
+  return (
     <div>
-      <h2>Your Role</h2>
-      <ul className="character-card-container"> {/* Add a container for the character cards */}
-        <h1>{data.name}</h1>
-          <li>
-            <div className="character-card"> {/* Wrap each character card in a div */}
-              <img
-                className="character-image"
-                src={`data:${data.char.image.contentType};base64,${arrayBufferToBase64(data.char.image.data.data)}`}
-                alt={data.char.name}
-              />
-              <div className="character-info">
-                <h3 className="character-name">{data.char.name}</h3>
-                <p className="character-ability">Ability: {data.char.ability}</p>
-              </div>
-            </div>
-          </li>
-       
-      </ul>
+      <h2>Character List</h2>
+     <GameCard playerChar={charId} />
     </div>
-    )
+  );
 }
 
 function arrayBufferToBase64(arrayBuffer) {
